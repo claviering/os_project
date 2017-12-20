a147fbbe8f5586a57aef88d9bb8f46e8921e66d2
claviering <claviering@gmail.com>
2017-12-20 23:30:24 +0800

    登录界面做好了

---
+++ fun.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = 'localhost:3306';  // mysql服务器主机地址
+$dbuser = 'root';            // mysql用户名
+$dbpass = 'uiop00o0';          // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+    die('Could not connect: ' . mysqli_error());
+}
+$tmp = "数据库连接成功!";
+echo $tmp;
+?>
